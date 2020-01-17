f8280cd055698ea5309fe94bbff7afef561ceeba
Mats Becker <mats.becker@gmail.com>
2020-01-17 10:20:09 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"natsclient adapter settings": {
+		"en": "Adapter settings for natsclient",
+		"de": "Adaptereinstellungen für natsclient",
+		"ru": "Настройки адаптера для natsclient",
+		"pt": "Configurações do adaptador para natsclient",
+		"nl": "Adapterinstellingen voor natsclient",
+		"fr": "Paramètres d'adaptateur pour natsclient",
+		"it": "Impostazioni dell'adattatore per natsclient",
+		"es": "Ajustes del adaptador para natsclient",
+		"pl": "Ustawienia adaptera dla natsclient",
+		"zh-cn": "natsclient的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
