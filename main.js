379fc8035bf246f9cf70874d32b3a79b58a05e6c
Mats Becker <mats.becker@gmail.com>
2020-01-17 13:45:31 +0100

    Changed async with promise

--- main.js
+++ main.js
@@ -12,192 +12,197 @@ const utils = require("@iobroker/adapter-core");
 // const fs = require("fs");
 
 class Natsclient extends utils.Adapter {
+  /**
+   * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+   */
+  constructor(options) {
+    super({
+      ...options,
+      name: "natsclient"
+    });
+    this.on("ready", this.onReady.bind(this));
+    this.on("objectChange", this.onObjectChange.bind(this));
+    this.on("stateChange", this.onStateChange.bind(this));
+    // this.on("message", this.onMessage.bind(this));
+    this.on("unload", this.onUnload.bind(this));
 
-	/**
-	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "natsclient",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("objectChange", this.onObjectChange.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
+    // Custom class parmeters
+    this.subscribedDevices = {};
+  }
 
-	/**
-	 * Get all deives from enum.antsclient to subscribe to
-	 * @returns {JSON}
-	 */
-	getSubscribedDevices() {		
-		return this.getEnum("natsclient", (err, result, _enum)  => {
-			const subscribedDevices = {}; // The devices from the enum.natsclient to subscribe
-			// this.log.info("--- getEnum ROOMS ---");
-			// this.log.info(JSON.stringify(err));
-			// this.log.info(JSON.stringify(result));
-			// this.log.info(JSON.stringify(_enum));
-			if(err !== null) {
-				return this.log.warn("getEnum('natsclient') error: " + err);
-			}
+  /**
+   * Get all deives from enum.antsclient to subscribe to
+   * @returns {JSON}
+   */
+  async getSubscribedDevices() {
+    return new Promise(resolve => {
+      this.getEnum("natsclient", (err, result, _enum) => {
+        // this.log.info("--- getEnum ROOMS ---");
+        // this.log.info(JSON.stringify(err));
+        // this.log.info(JSON.stringify(result));
+        // this.log.info(JSON.stringify(_enum));
+        if (err !== null) {
+          return this.log.warn("getEnum('natsclient') error: " + err);
+        }
 
-			// const _result = result["enum.natsclient"]; // Getting the enum "enum.natsclient"; creating temp variable _result to loop throug
-			for(const _key in result) {
-				// this.log.info("-----");
-				// this.log.info(JSON.stringify(result[_key]["common"]));
+        // const _result = result["enum.natsclient"]; // Getting the enum "enum.natsclient"; creating temp variable _result to loop throug
+        for (const _key in result) {
+          // this.log.info("-----");
+          // this.log.info(JSON.stringify(result[_key]["common"]));
 
-				// Create key in object subscribed devices and initialie ezmpty array
-				subscribedDevices[_key] = [];
+          // Create key in object subscribed devices and initialie ezmpty array
+          this.subscribedDevices[_key] = [];
 
-				const _enum = result[_key]; // Temporary variable for enum object in enum.natsclient
-				if(typeof _enum["common"] !== "undefined" && typeof _enum["common"]["members"] !== "undefined" && _enum["common"]["members"].length > 0) {
-					// this.log.info("Devices: " + _enum["common"]["members"]);
-					const devices = _enum["common"]["members"];
-					devices.forEach(device => subscribedDevices[_key].push(device));
-				}
-			}
-			return subscribedDevices;
-		});
-	}
+          const _enum = result[_key]; // Temporary variable for enum object in enum.natsclient
+          if (
+            typeof _enum["common"] !== "undefined" &&
+            typeof _enum["common"]["members"] !== "undefined" &&
+            _enum["common"]["members"].length > 0
+          ) {
+            // this.log.info("Devices: " + _enum["common"]["members"]);
+            const devices = _enum["common"]["members"];
+            devices.forEach(device => this.subscribedDevices[_key].push(device));
+          }
+        }
+        resolve()
+      });
+    });
+  }
 
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
+  /**
+   * Is called when databases are connected and adapter received configuration.
+   */
+  async onReady() {
+    // Initialize your adapter here
 
-		// Reset the connection indicator during startup
-		this.setState("info.connection", false, true);
+    // Reset the connection indicator during startup
+    this.setState("info.connection", false, true);
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
-		this.log.info("config natsconnection: " + this.config.natsconnection);
+    // The adapters config (in the instance object everything under the attribute "native") is accessible via
+    // this.config:
+    this.log.info("config option1: " + this.config.option1);
+    this.log.info("config option2: " + this.config.option2);
+    this.log.info("config natsconnection: " + this.config.natsconnection);
 
-		if(this.config.natsconnection.length > 0) {
-			await this.setStateAsync("info.connection", true);
-		}
+    if (this.config.natsconnection.length > 0) {
+      await this.setStateAsync("info.connection", true);
+    }
 
-		const subscribedDevices = this.getSubscribedDevices();
-		this.log.info("--- subscribed devices ---");
-		this.log.info(subscribedDevices);
+    await this.getSubscribedDevices();
+    this.log.info("--- subscribed devices ---");
+    this.log.info(this.subscribedDevices);
 
-
-		/*
+    /*
 		For every state in the system there has to be also an object of type state
 		Here a simple template for a boolean variable named "testVariable"
 		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
 		*/
-		await this.setObjectAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+    await this.setObjectAsync("testVariable", {
+      type: "state",
+      common: {
+        name: "testVariable",
+        type: "boolean",
+        role: "indicator",
+        read: true,
+        write: true
+      },
+      native: {}
+    });
 
-		// in this template all states changes inside the adapters namespace are subscribed
-		this.subscribeStates("*");
+    // in this template all states changes inside the adapters namespace are subscribed
+    this.subscribeStates("*");
 
-		/*
+    /*
 		setState examples
 		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
 		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+    // the variable testVariable is set to true as command (ack=false)
+    await this.setStateAsync("testVariable", true);
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+    // same thing, but the value is flagged "ack"
+    // ack should be always set to true if the value is received from or acknowledged from the target system
+    await this.setStateAsync("testVariable", { val: true, ack: true });
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		// await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+    // same thing, but the state is deleted after 30s (getState will return null afterwards)
+    // await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
+    // examples for the checkPassword/checkGroup functions
+    let result = await this.checkPasswordAsync("admin", "iobroker");
+    this.log.info("check user admin pw iobroker: " + result);
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
-	}
+    result = await this.checkGroupAsync("admin", "admin");
+    this.log.info("check group user admin group admin: " + result);
+  }
 
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			this.log.info("cleaned everything up...");
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
+  /**
+   * Is called when adapter shuts down - callback has to be called under any circumstances!
+   * @param {() => void} callback
+   */
+  onUnload(callback) {
+    try {
+      this.log.info("cleaned everything up...");
+      callback();
+    } catch (e) {
+      callback();
+    }
+  }
 
-	/**
-	 * Is called if a subscribed object changes
-	 * @param {string} id
-	 * @param {ioBroker.Object | null | undefined} obj
-	 */
-	onObjectChange(id, obj) {
-		if (obj) {
-			// The object was changed
-			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-		} else {
-			// The object was deleted
-			this.log.info(`object ${id} deleted`);
-		}
-	}
+  /**
+   * Is called if a subscribed object changes
+   * @param {string} id
+   * @param {ioBroker.Object | null | undefined} obj
+   */
+  onObjectChange(id, obj) {
+    if (obj) {
+      // The object was changed
+      this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+    } else {
+      // The object was deleted
+      this.log.info(`object ${id} deleted`);
+    }
+  }
 
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
+  /**
+   * Is called if a subscribed state changes
+   * @param {string} id
+   * @param {ioBroker.State | null | undefined} state
+   */
+  onStateChange(id, state) {
+    if (state) {
+      // The state was changed
+      this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+    } else {
+      // The state was deleted
+      this.log.info(`state ${id} deleted`);
+    }
+  }
 
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.message" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
+  // /**
+  //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+  //  * Using this method requires "common.message" property to be set to true in io-package.json
+  //  * @param {ioBroker.Message} obj
+  //  */
+  // onMessage(obj) {
+  // 	if (typeof obj === "object" && obj.message) {
+  // 		if (obj.command === "send") {
+  // 			// e.g. send email or pushover or whatever
+  // 			this.log.info("send command");
 
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
+  // 			// Send response in callback if required
+  // 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+  // 		}
+  // 	}
+  // }
 }
 
 // @ts-ignore parent is a valid property on module
 if (module.parent) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new Natsclient(options);
+  // Export the constructor in compact mode
+  /**
+   * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+   */
+  module.exports = options => new Natsclient(options);
 } else {
-	// otherwise start the instance directly
-	new Natsclient();
+  // otherwise start the instance directly
+  new Natsclient();
 }
