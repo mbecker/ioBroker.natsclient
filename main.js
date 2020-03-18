a9a7de64a0999d639e4d5dcca595dbb777d2533f
Mats Becker <mats.becker@gmail.com>
2020-03-18 16:33:13 +0100

    added

--- main.js
+++ main.js
@@ -9,6 +9,8 @@
 const utils = require("@iobroker/adapter-core");
 const NATS = require("nats");
 const async = require("async");
+const _ = require("lodash");
+const YAML = require("json-to-pretty-yaml");
 
 // TODO: Replace const with config
 const initget = "init.get";
@@ -200,6 +202,23 @@ class Natsclient extends utils.Adapter {
   }
 
 
+  createObject(array, tmpObj) {
+    if (
+      array[0].toLowerCase() === "configuration"
+    ) {
+      return;
+    }
+    if (array.length === 1) {
+      const obj = {};
+      obj[array[0]] = tmpObj;
+      return obj;
+    } else {
+      const obj = {};
+      obj[array[0]] = this.createObject(array.splice(1), tmpObj);
+      return obj;
+    }
+  }
+
   /**
    * Is called when databases are connected and adapter received configuration.
    */
@@ -216,6 +235,7 @@ class Natsclient extends utils.Adapter {
 
     const objects = this.config.getobjectsid.split(",");
     let arr = [];
+    let objOrigin = {};
 
     objects.forEach(el => {
       this.getForeignObjects(el, (err, obj) => {
@@ -230,52 +250,114 @@ class Natsclient extends utils.Adapter {
           if (typeof obj[k]["common"] === "undefined") continue;
           if (typeof obj[k]["common"]["type"] === "undefined") continue;
           if (typeof obj[k]["common"]["role"] === "undefined") continue;
-          let role = obj[k]["common"]["role"].toLocaleLowerCase();
+          let role = obj[k]["common"]["role"].toLowerCase();
 
-          if (!role.includes("switch.") && !role.includes("value.") && !role.includes("sensor.") 
-          && !k.toLocaleLowerCase().includes("temperature")
-          && !k.toLocaleLowerCase().includes("humidity")
-          && !k.toLocaleLowerCase().includes("pressure")
-          && !k.toLocaleLowerCase().includes("open")
-          && !k.toLocaleLowerCase().includes("daylight")
-          && !k.toLocaleLowerCase().includes("dark")
-          && !k.toLocaleLowerCase().includes("lux")
-          && !k.toLocaleLowerCase().includes("lightlevel")
+          if (!role.includes("switch.") && !role.includes("value.") && !role.includes("sensor.") && !role.includes("indicator.")
+            && !k.toLowerCase().includes("temperature")
+            && !k.toLowerCase().includes("humidity")
+            && !k.toLowerCase().includes("pressure")
+            && !k.toLowerCase().includes("open")
+            && !k.toLowerCase().includes("reachable")
+            && !k.toLowerCase().includes("daylight")
+            && !k.toLowerCase().includes("dark")
+            && !k.toLowerCase().includes("lux")
+            && !k.toLowerCase().includes("lightlevel")
+            && !k.toLowerCase().includes("buttonpressed")
+            && !k.toLowerCase().includes("switch")
+            && !k.toLowerCase().includes("electric_-_w") // zwave.0.NODE4.METER.Electric_-_W_1 : Power for zwave power plug
+            && !k.toLowerCase().includes("electric_-_kwh") // zwave.0.NODE4.METER.Electric_-_kWh_1 : Power for zwave power plug
+            && !k.toLowerCase().includes("power") // zwave.0.NODE4.SENSOR_MULTILEVEL.Power_1
+
+            || k.toLowerCase().includes("_info") // tado.0.466027._info
           ) continue;
+
+          // Tado
+
 
           if (role.includes("switch.")) role = role.split("switch.")[1];
           if (role.includes("value.")) role = role.split("value.")[1];
           if (role.includes("sensor.")) role = role.split("sensor.")[1];
-          if (k.toLocaleLowerCase().includes("temperature")) role = "temperature";
-          if (k.toLocaleLowerCase().includes("humidity")) role = "humidity";
-          if (k.toLocaleLowerCase().includes("pressure")) role = "pressure";
-          if (k.toLocaleLowerCase().includes("open")) role = "open";
-          if (k.toLocaleLowerCase().includes("daylight")) role = "daylight";
-          if (k.toLocaleLowerCase().includes("dark")) role = "dark";
-          if (k.toLocaleLowerCase().includes("lux")) role = "lux";
-          if (k.toLocaleLowerCase().includes("lightlevel")) role = "lightlevel";
-          if (k.toLocaleLowerCase().includes("lastupdated")) role = "lastupdated";
-          if (role.toLocaleLowerCase().includes("datetime")) role = "lastupdated";
+          if (role.includes("indicator.")) role = role.split("indicator.")[1];
+          if (k.toLowerCase().includes("temperature")) role = "temperature";
+          if (k.toLowerCase().includes("humidity")) role = "humidity";
+          if (k.toLowerCase().includes("pressure")) role = "pressure";
+          if (k.toLowerCase().includes("openwindow")) role = "openWindow";
+          if (k.toLowerCase().includes("openwindowdetection")) role = "openwindowdetection";          
+          if (k.toLowerCase().includes("pushnotifications")) role = "device_setting";
+          if (k.toLowerCase().includes("daylight")) role = "daylight";
+          if (k.toLowerCase().includes("dark")) role = "dark";
+          if (k.toLowerCase().includes("lux")) role = "lux";
+          if (k.toLowerCase().includes("lightlevel")) role = "lightlevel";
+          if (k.toLowerCase().includes("lastupdated")) role = "lastupdated";
+          if (k.toLowerCase().includes("datetime")) role = "lastupdated";
+          if (k.toLowerCase().includes("electric_-_w")) role = "electricwatt";
+          if (k.toLowerCase().includes("electric_-_kwh")) role = "electrickwh";
+          if (k.toLowerCase().includes("power")) role = "power";
+          if (k.toLowerCase().includes("buttonpressed")) role = "control";
 
+          if (role.toLowerCase().includes("switch") || k.toLowerCase().includes("switch")) {
+            role = "switch";
+            // shelly.0.SHSW-1#114418#1
+            if (role.toLowerCase().includes("shelley")) role = "light";
+          }
 
+          const splitted = k.toLowerCase().split(".");
           obj2[k] = {};
           obj2[k]["id"] = k;
           obj2[k]["field"] = role.replace(".", "_"); // role
+          obj2[k]["tags"] = {};
+          obj2[k]["tags"]["name"] = splitted[splitted.length - 1];
+          obj2[k]["tags"]["type"] = obj[k]["common"]["type"];
 
           // An object like "switch.power" has maybe states like "on", "off"
           // These values could be later used as true/false
           // Chekc that that object has these states and the set the type to "onoff"; the original type is maybe "mixed"
-          obj2[k]["tags"] = {};
-          obj2[k]["tags"]["type"] = obj[k]["common"]["type"];
-          if (typeof obj[k]["common"]["states"] !== "undefined") {
+          
+          
+          if (k.toLowerCase().includes("pushnotifications")) {
+            obj2[k]["tags"]["device_setting"] = "pushnotifications";
+            
+            obj2[k]["tags"]["pushnotifications"] = splitted[splitted.length - 1];
+            obj2[k]["tags"]["device"] = splitted[4];
+          }
+
+          
+
+
+          if(role === "openwindowdetection") {
+            obj2[k]["tags"]["openwindowdetection"] = splitted[splitted.length - 1];
+            if(splitted[splitted.length - 1] === "enabled") obj2[k]["tags"]["type"] = "boolean";
+          }
+
+          if(role === "openWindow") {
+            obj2[k]["tags"]["openWindow"] = splitted[splitted.length - 1];
+          }
+
+          if(k.toLowerCase().includes("detectedtime")) obj2[k]["tags"]["type"] = "datetime";
+          if(k.toLowerCase().includes("inseconds")) obj2[k]["tags"]["type"] = "number";
+          if(k.toLowerCase().includes("expiry")) obj2[k]["tags"]["type"] = "number";
+          if(splitted[splitted.length - 1] === "open") obj2[k]["field"] = "windowdoor";
+          if (k.toLowerCase().includes("buttonpressed")) obj2[k]["tags"]["type"] = "number";
+          if (role === "daylight") obj2[k]["tags"]["type"] = "boolean";
+
+          
+          if (typeof obj[k]["common"]["states"] !== "undefined" && obj[k]["common"]["type"] !== "boolean") {
             const states = obj[k]["common"]["states"];
-            for (const kstates in states) {
-              const vstates = states[kstates].toLocaleLowerCase();
-              if (vstates.includes("on")) {
-                obj2[k]["tags"]["type"] = "onoff";
-                obj2[k]["tags"]["unit"] = "onoff";
-              }
+            if (typeof obj2[k]["states"] !== "undefined") obj2[k]["states"] = {};
+            obj2[k]["states"] = states;
+
+            const objectKeys = Object.keys(states);
+            if(objectKeys.length === 2 && (objectKeys[0].toLowerCase() === "on" || objectKeys[1].toLowerCase() === "on")) {
+              obj2[k]["tags"]["type"] = "onoff";
             }
+
+            // for (const kstates in states) {
+            //   const vstates = states[kstates].toLowerCase();
+            //   if (vstates.includes("on")) {
+            //     obj2[k]["tags"]["type"] = "onoff";
+            //     obj2[k]["tags"]["unit"] = "onoff";
+            //   }
+            // }
           }
           if (role === "lastupdated") {
             obj2[k]["tags"]["type"] = "datetime";
@@ -286,31 +368,62 @@ class Natsclient extends utils.Adapter {
           if (obj[k]["common"]["unit"] === "°C") obj2[k]["tags"]["unit"] = "celcius";
           if (obj[k]["common"]["unit"] === "%") obj2[k]["tags"]["unit"] = "percentage";
 
-          if (k.toLocaleLowerCase().includes("setting")) {
+          if (k.toLowerCase().includes("setting")) {
             obj2[k]["tags"]["setting"] = true;
           } else {
             obj2[k]["tags"]["setting"] = false;
           }
-          if (k.toLocaleLowerCase().includes("rooms")) {
-            const ks = k.toLocaleLowerCase().split(".");
+          if (k.toLowerCase().includes("rooms")) {
+            const ks = k.toLowerCase().split(".");
             const room = ks[4];
             obj2[k]["tags"]["room"] = room;
           }
           // {"enum.rooms.esszimmer":"Esszimmer"}
           const enums = obj[k].enums;
-          for(const enumKey in enums) {
-            if(enumKey.toLocaleLowerCase().includes("rooms")) obj2[k]["tags"]["room"] = enums[enumKey];
+          for (const enumKey in enums) {
+            if (enumKey.toLowerCase().includes("rooms")) obj2[k]["tags"]["room"] = enums[enumKey];
           }
-          if(typeof obj2[k]["tags"]["room"] === "object") {
-            for(const enumKey in  obj2[k]["tags"]["room"]) {
-              if(enumKey.toLocaleLowerCase().includes("en")) obj2[k]["tags"]["room"] =  obj2[k]["tags"]["room"]["en"];
-            } 
+          if (typeof obj2[k]["tags"]["room"] === "object") {
+            for (const enumKey in obj2[k]["tags"]["room"]) {
+              if (enumKey.toLowerCase().includes("en")) obj2[k]["tags"]["room"] = obj2[k]["tags"]["room"]["en"];
+            }
           }
 
           arr.push(obj2[k]);
-          const objs = JSON.stringify(arr);
+
+          const ks = k.toLowerCase().split(".");
+          const ksObj = this.createObject(ks, obj2[k]);
+          objOrigin = _.merge(objOrigin, ksObj);
+
+          const objs = JSON.stringify(objOrigin);
           this.setState("info.objs", objs);
           this.log.info("--- WRITE DONE !!! ---");
+          this.log.info(__dirname);
+          const outputLocation = require("path").resolve(__dirname, "data.json");
+          require("fs").writeFile(outputLocation, JSON.stringify(objOrigin, null, 4), (err) => {
+            if (err) {
+              this.log.error(err);
+            } else {
+              this.log.info("JSON saved to " + outputLocation);
+            }
+          });
+          const outputLocation2 = require("path").resolve(__dirname, "arr.json");
+          require("fs").writeFile(outputLocation2, JSON.stringify(arr, null, 4), (err) => {
+            if (err) {
+              this.log.error(err);
+            } else {
+              this.log.info("JSON saved to " + outputLocation);
+            }
+          });
+          const data = YAML.stringify(arr);
+          const outputLocation3 = require("path").resolve(__dirname, "arr.yaml");
+          require("fs").writeFile(outputLocation3, data, (err) => {
+            if (err) {
+              this.log.error(err);
+            } else {
+              this.log.info("JSON saved to " + outputLocation);
+            }
+          });
 
 
           // this.getForeignStateAsync(k)
